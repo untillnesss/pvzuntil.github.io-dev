b26b4f798482270b1cd5cf2be34259506882a4ad
pvzuntil <33585758+pvzuntil@users.noreply.github.com>
2019-11-05 08:20:36 +0700

    First

---
+++ src/components/AppBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Muhammad Abdullah Sa'id</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn text>Home</v-btn>
+            <v-btn text>About</v-btn>
+            <v-btn text>Achivement</v-btn>
+            <v-btn text>Project</v-btn>
+        </v-toolbar-items>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-parallax
+    dark
+    src="@/assets/banner.jpg"
+    height="550"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
