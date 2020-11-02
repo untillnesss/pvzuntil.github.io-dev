93f218c8f5f54f10a442b6ddc4dee7092dbc8ed5
untillness <pvauntil@gmail.com>
2020-11-02 09:01:25 +0700

    change: rework backend

---
+++ src/components/loginPage/loginField.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col class="d-flex flex-column justify-center align-center">
+        <v-card width="500">
+          <v-card-title class="blue white--text">
+            <div>
+              <h3 class="headline mb-0 font-weight-bold">LOGIN ADMIN !</h3>
+              <div class="caption">kalo bukan admin, jangan login yeee...</div>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Username"
+                  outlined
+                  dense
+                  hide-details
+                  rounded
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password"
+                  outlined
+                  dense
+                  hide-details
+                  rounded
+                  type="password"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="d-flex justify-end">
+            <span>
+              <v-btn text color="error" to="/">Kembali</v-btn>
+              <v-btn color="primary">Masuk</v-btn>
+            </span>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,12 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <v-row align="center" justify="center">
-      <v-col class="d-flex flex-column justify-center align-center">
-        <v-btn color="primary" @click="loginGoogle()">LOGIN GOOGLE</v-btn>
-        <div v-if="!isGranted" class="d-flex flex-column justify-center align-center">
-          <p class="mt-4 red--text">AKSES DITOLAK</p>
-          <div style="width: 150%">
-            <v-divider></v-divider>
-          </div>
-          <small class="mt-2">Hanya admin yang boleh masuk.</small>
-        </div>
-        <small class="mt-3">
-          <router-link to="/">
-            <a>Kembali !</a>
-          </router-link>
-        </small>
-      </v-col>
-    </v-row>
-  </v-container>
+  <LoginFieldVue />
 </template>
 
 <script>
-import firebase from "firebase";
+import LoginFieldVue from "../components/loginPage/loginField.vue";
 export default {
-  data() {
-    return {
-      isGranted: true
-    };
+  components: {
+    LoginFieldVue
   },
-  methods: {
-    loginGoogle() {
-      let provider = new firebase.auth.GoogleAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(he => {
-          if (he.user.email != "pvzuntil@gmail.com") {
-            this.isGranted = false;
-          } else {
-            this.$router.replace("/admin");
-          }
-        });
-    }
-  }
 };
 </script>
